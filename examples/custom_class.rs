f9147c25c7e1ab430ddfbcdeac90460c22bb582e
Steven Sheldon <steven@sasheldon.com>
2014-09-01 11:26:54 -0700

    Added custom class example.

---
+++ examples/custom_class.rs
@@ -0,0 +1,63 @@
+#![feature(phase)]
+
+#[phase(plugin, link)]
+extern crate objc;
+
+use objc::{class, ClassDecl, Id};
+use objc::runtime::Object;
+use objc::foundation::{INSObject, NSObject};
+
+object_struct!(MYObject)
+
+impl MYObject {
+	fn number(&self) -> uint {
+		let obj = unsafe {
+			&*(self as *const _ as *const Object)
+		};
+		unsafe {
+			*obj.get_ivar::<uint>("_number")
+		}
+	}
+
+	fn set_number(&mut self, number: uint) {
+		let obj = unsafe {
+			&mut *(self as *mut _ as *mut Object)
+		};
+		unsafe {
+			obj.set_ivar("_number", number);
+		}
+	}
+
+	fn register() {
+		let superclass = class::<NSObject>();
+		let mut decl = ClassDecl::new(superclass, "MYObject").unwrap();
+
+		decl.add_ivar::<uint>("_number");
+		decl.add_method(method!(
+			(&mut MYObject)this, setNumber:(uint)number; {
+				this.set_number(number);
+			}
+		));
+		decl.add_method(method!(
+			(&MYObject)this, number -> uint {
+				this.number()
+			}
+		));
+
+		decl.register();
+	}
+}
+
+fn main() {
+	MYObject::register();
+	let mut obj: Id<MYObject> = INSObject::new();
+	println!("{}", obj);
+
+	obj.deref_mut().set_number(7);
+	println!("Number: {}", obj.number());
+
+	unsafe {
+		msg_send![obj setNumber:12u];
+	}
+	println!("Number: {}", obj.number());
+}

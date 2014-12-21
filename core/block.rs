401631ad0e871b99975e7b6bca66ff7051ff2c86
Steven Sheldon <steven@sasheldon.com>
2014-12-21 03:00:42 -0600

    Renamed Block to ConcreteBlock.

--- core/block.rs
+++ core/block.rs
@@ -14,18 +14,18 @@ extern {
 }
 
 #[repr(C)]
-struct Block<C: Clone> {
+struct ConcreteBlock<C: Clone> {
     isa: *const Class,
     flags: c_int,
     _reserved: c_int,
-    pub invoke: unsafe extern fn(&mut Block<C>, ...),
+    pub invoke: unsafe extern fn(&mut ConcreteBlock<C>, ...),
     descriptor: *const BlockDescriptor<C>,
     pub context: C,
 }
 
-impl<C: Clone> Block<C> {
-    pub fn new(invoke: unsafe extern fn(&mut Block<C>, ...), context: C) -> Block<C> {
-        Block {
+impl<C: Clone> ConcreteBlock<C> {
+    pub fn new(invoke: unsafe extern fn(&mut ConcreteBlock<C>, ...), context: C) -> ConcreteBlock<C> {
+        ConcreteBlock {
             isa: &_NSConcreteStackBlock,
             // 1 << 25 = BLOCK_HAS_COPY_DISPOSE
             flags: 1 << 25,
@@ -39,29 +39,29 @@ impl<C: Clone> Block<C> {
         }
     }
 
-    pub fn copy(&self) -> Id<Block<C>> {
+    pub fn copy(&self) -> Id<ConcreteBlock<C>> {
         unsafe {
-            let block = msg_send![self copy] as *mut Block<C>;
+            let block = msg_send![self copy] as *mut ConcreteBlock<C>;
             Id::from_retained_ptr(block)
         }
     }
 }
 
-impl<C: Clone> Message for Block<C> { }
+impl<C: Clone> Message for ConcreteBlock<C> { }
 
-impl<C: Clone> Clone for Block<C> {
-    fn clone(&self) -> Block<C> {
-        Block::new(self.invoke, self.context.clone())
+impl<C: Clone> Clone for ConcreteBlock<C> {
+    fn clone(&self) -> ConcreteBlock<C> {
+        ConcreteBlock::new(self.invoke, self.context.clone())
     }
 }
 
-unsafe extern fn block_context_dispose<C: Clone>(block: &mut Block<C>) {
+unsafe extern fn block_context_dispose<C: Clone>(block: &mut ConcreteBlock<C>) {
     let mut context = mem::uninitialized();
     ptr::copy_nonoverlapping_memory(&mut context, &block.context, 1);
     drop(context);
 }
 
-unsafe extern fn block_context_copy<C: Clone>(dst: &mut Block<C>, src: &Block<C>) {
+unsafe extern fn block_context_copy<C: Clone>(dst: &mut ConcreteBlock<C>, src: &ConcreteBlock<C>) {
     dst.context = src.context.clone();
 }
 
@@ -69,15 +69,15 @@ unsafe extern fn block_context_copy<C: Clone>(dst: &mut Block<C>, src: &Block<C>
 struct BlockDescriptor<C: Clone> {
     _reserved: c_ulong,
     block_size: c_ulong,
-    copy_helper: unsafe extern fn(&mut Block<C>, &Block<C>),
-    dispose_helper: unsafe extern fn(&mut Block<C>),
+    copy_helper: unsafe extern fn(&mut ConcreteBlock<C>, &ConcreteBlock<C>),
+    dispose_helper: unsafe extern fn(&mut ConcreteBlock<C>),
 }
 
 impl<C: Clone> BlockDescriptor<C> {
     pub fn new() -> BlockDescriptor<C> {
         BlockDescriptor {
             _reserved: 0,
-            block_size: mem::size_of::<Block<C>>() as c_ulong,
+            block_size: mem::size_of::<ConcreteBlock<C>>() as c_ulong,
             copy_helper: block_context_copy::<C>,
             dispose_helper: block_context_dispose::<C>,
         }
@@ -88,16 +88,16 @@ impl<C: Clone> BlockDescriptor<C> {
 mod tests {
     use std::mem;
     use objc_test_utils::invoke_int_block;
-    use super::Block;
+    use super::ConcreteBlock;
 
     #[test]
     fn test_create_block() {
-        extern fn block_get_int(block: &Block<int>) -> int {
+        extern fn block_get_int(block: &ConcreteBlock<int>) -> int {
             block.context
         }
 
         let result = unsafe {
-            let block = Block::new(mem::transmute(block_get_int), 13i);
+            let block = ConcreteBlock::new(mem::transmute(block_get_int), 13i);
             invoke_int_block(mem::transmute(&block))
         };
         assert!(result == 13);

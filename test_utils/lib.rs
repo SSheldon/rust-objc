d7c8861aa77ce313de0d1051dadd2b55e784b6a2
Steven Sheldon <steven@sasheldon.com>
2015-01-10 15:51:58 -0800

    Added documentation and tests for test_utils.

--- test_utils/lib.rs
+++ test_utils/lib.rs
@@ -1,17 +1,53 @@
 #![crate_name = "objc_test_utils"]
 #![crate_type = "lib"]
 
-#[allow(unstable)]
-extern crate libc;
+#![allow(unstable)]
 
-use libc::c_void;
+#[link(name = "Foundation", kind = "framework")]
+extern { }
+
+/// A block that takes no arguments and returns an integer: `int32_t (^)()`.
+#[allow(missing_copy_implementations)]
+pub enum IntBlock { }
+
+/// A block that takes one integer argument, adds to it, and returns the sum:
+/// `int32_t (^)(int32_t)`.
+#[allow(missing_copy_implementations)]
+pub enum AddBlock { }
 
 #[link(name="block_utils", kind="static")]
 extern {
-    pub fn get_int_block() -> *mut c_void;
-    pub fn get_int_block_with(i: i32) -> *mut c_void;
-    pub fn get_add_block() -> *mut c_void;
-    pub fn get_add_block_with(i: i32) -> *mut c_void;
-    pub fn invoke_int_block(block: *mut c_void) -> i32;
-    pub fn invoke_add_block(block: *mut c_void, a: i32) -> i32;
+    /// Returns a pointer to a global `IntBlock` that returns 7.
+    pub fn get_int_block() -> *mut IntBlock;
+    /// Returns a pointer to a copied `IntBlock` that returns `i`.
+    pub fn get_int_block_with(i: i32) -> *mut IntBlock;
+    /// Returns a pointer to a global `AddBlock` that returns its argument + 7.
+    pub fn get_add_block() -> *mut AddBlock;
+    /// Returns a pointer to a copied `AddBlock` that returns its argument + `i`.
+    pub fn get_add_block_with(i: i32) -> *mut AddBlock;
+    /// Invokes an `IntBlock` and returns its result.
+    pub fn invoke_int_block(block: *mut IntBlock) -> i32;
+    /// Invokes an `AddBlock` with `a` and returns the result.
+    pub fn invoke_add_block(block: *mut AddBlock, a: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_int_block() {
+        unsafe {
+            assert!(invoke_int_block(get_int_block()) == 7);
+            assert!(invoke_int_block(get_int_block_with(13)) == 13);
+        }
+    }
+
+    #[test]
+    fn test_add_block() {
+        unsafe {
+            assert!(invoke_add_block(get_add_block(), 5) == 12);
+            assert!(invoke_add_block(get_add_block_with(3), 5) == 8);
+        }
+    }
 }

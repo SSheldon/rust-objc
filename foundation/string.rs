6221225c7e83e03fbc3ecd9240847dbd44ead12c
Steven Sheldon <steven@sasheldon.com>
2015-01-25 13:05:35 -0800

    Added NSString tests.

--- foundation/string.rs
+++ foundation/string.rs
@@ -1,5 +1,5 @@
-use std::ffi;
 use std::mem;
+use std::slice;
 use std::str;
 
 use objc::Id;
@@ -28,11 +28,23 @@ pub trait INSMutableCopying : INSObject {
     }
 }
 
+static UTF8_ENCODING: uint = 4;
+
 pub trait INSString : INSObject {
-    fn as_str(&self) -> &str {
+    fn len(&self) -> uint {
         unsafe {
-            let result: *const i8 = msg_send![self, UTF8String];
-            let bytes = ffi::c_str_to_bytes(&result);
+            msg_send![self, lengthOfBytesUsingEncoding:UTF8_ENCODING]
+        }
+    }
+
+    fn as_str(&self) -> &str {
+        let bytes = unsafe {
+            let bytes: *const i8 = msg_send![self, UTF8String];
+            bytes as *const u8
+        };
+        let len = self.len();
+        unsafe {
+            let bytes = slice::from_raw_buf(&bytes, len);
             let s = str::from_utf8(bytes).unwrap();
             mem::transmute(s)
         }
@@ -40,12 +52,11 @@ pub trait INSString : INSObject {
 
     fn from_str(string: &str) -> Id<Self> {
         let cls = <Self as INSObject>::class();
-        let utf8_encoding = 4u;
         unsafe {
             let obj: *mut Self = msg_send![cls, alloc];
             let obj: *mut Self = msg_send![obj, initWithBytes:string.as_ptr()
                                                        length:string.len()
-                                                     encoding:utf8_encoding];
+                                                     encoding:UTF8_ENCODING];
             Id::from_retained_ptr(obj)
         }
     }
@@ -62,5 +73,34 @@ impl INSCopying for NSString {
 impl Str for NSString {
     fn as_slice(&self) -> &str {
         self.as_str()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use objc::Id;
+    use super::{INSCopying, INSString, NSString};
+
+    #[test]
+    fn test_utf8() {
+        let expected = "ประเทศไทย中华Việt Nam";
+        let s: Id<NSString> = INSString::from_str(expected);
+        assert!(s.len() == expected.len());
+        assert!(s.as_str() == expected);
+    }
+
+    #[test]
+    fn test_interior_nul() {
+        let expected = "Hello\0World";
+        let s: Id<NSString> = INSString::from_str(expected);
+        assert!(s.len() == expected.len());
+        assert!(s.as_str() == expected);
+    }
+
+    #[test]
+    fn test_copy() {
+        let s: Id<NSString> = INSString::from_str("Hello!");
+        let copied = s.copy();
+        assert!(copied.as_str() == s.as_str());
     }
 }

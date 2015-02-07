657d63b0f7d660c0e78466088c16e2a10d6c4092
Steven Sheldon <steven@sasheldon.com>
2015-02-07 14:17:38 -0800

    Improve block module documentation.

--- core/block.rs
+++ core/block.rs
@@ -1,7 +1,46 @@
-//! A Rust interface for Objective-C blocks.
-//!
-//! For more information on the specifics of the block implementation, see
-//! Clang's documentation: http://clang.llvm.org/docs/Block-ABI-Apple.html
+/*!
+A Rust interface for Objective-C blocks.
+
+For more information on the specifics of the block implementation, see
+Clang's documentation: http://clang.llvm.org/docs/Block-ABI-Apple.html
+
+# Invoking blocks
+
+The `Block` struct is used for invoking blocks from Objective-C. For example,
+consider this Objective-C function:
+
+``` objc
+int32_t sum(int32_t (^block)(int32_t, int32_t)) {
+    return block(5, 8);
+}
+```
+
+We could write it in Rust as the following:
+
+```
+fn sum(block: &mut Block<(i32, i32), i32>) {
+    block.call((5, 8))
+}
+```
+
+Note the extra parentheses in the `call` method, since the arguments must be
+passed as a tuple.
+
+# Creating blocks
+
+Creating a block to pass to Objective-C can be done with the `ConcreteBlock`
+struct. For example, to create a block that adds two `i32`s, we could write:
+
+```
+let block = ConcreteBlock::new(|a: i32, b: i32| a + b);
+let block = block.copy();
+```
+
+It is important to copy your block to the heap (with the `copy` method) before
+passing it to Objective-C; this is because our `ConcreteBlock` is only meant
+to be copied once, and we can enforce this in Rust, but if Objective-C code
+were to copy it twice we could have a double free.
+*/
 
 use std::mem;
 use std::ops::{Deref, DerefMut};

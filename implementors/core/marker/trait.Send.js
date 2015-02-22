047ce124d7e7919c3ae994cf11b52b40f2fa514f
Steven Sheldon <steven@sasheldon.com>
2015-02-22 14:11:21 -0800

    Updated documentation.

---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['objc'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='objc/struct.Id.html' title='objc::Id'>Id</a>&lt;T, <a class='enum' href='objc/enum.Owned.html' title='objc::Owned'>Owned</a>&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='objc/struct.Id.html' title='objc::Id'>Id</a>&lt;T, <a class='enum' href='objc/enum.Shared.html' title='objc::Shared'>Shared</a>&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='objc/struct.WeakId.html' title='objc::WeakId'>WeakId</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['objc'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T, O&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='objc/struct.Id.html' title='objc::Id'>Id</a>&lt;T, O&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='objc/struct.WeakId.html' title='objc::WeakId'>WeakId</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

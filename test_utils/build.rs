b5d2cc040117241ad75e224502be8d066bda0b06
Steven Sheldon <steven@sasheldon.com>
2015-02-24 00:23:09 -0800

    Updated test_utils to gcc 0.3.

--- test_utils/build.rs
+++ test_utils/build.rs
@@ -1,8 +1,5 @@
 extern crate gcc;
 
-use std::default::Default;
-
 fn main() {
-    let config = Default::default();
-    gcc::compile_library("libblock_utils.a", &config, &["block_utils.m"]);
+    gcc::compile_library("libblock_utils.a", &["block_utils.m"]);
 }

3c6f0ff987b322a71ddd3daa7c2e30f5b501a237
Steven Sheldon <steven@sasheldon.com>
2015-03-08 01:32:02 -0800

    Removed Encode impl for NSObjects.

--- foundation/macros.rs
+++ foundation/macros.rs
@@ -56,29 +56,5 @@ macro_rules! object_impl {
     );
     ($name:ident, $($t:ident),*) => (
         unsafe impl<$($t),*> ::objc::Message for $name<$($t),*> { }
-
-        impl<'a $(, $t)*> ::objc::Encode for &'a $name<$($t),*> {
-            fn code() -> &'static str { "@" }
-        }
-
-        impl<'a $(, $t)*> ::objc::Encode for &'a mut $name<$($t),*> {
-            fn code() -> &'static str { "@" }
-        }
-
-        impl<'a $(, $t)*> ::objc::Encode for Option<&'a $name<$($t),*>> {
-            fn code() -> &'static str { "@" }
-        }
-
-        impl<'a $(, $t)*> ::objc::Encode for Option<&'a mut $name<$($t),*>> {
-            fn code() -> &'static str { "@" }
-        }
-
-        impl<$($t),*> ::objc::Encode for *const $name<$($t),*> {
-            fn code() -> &'static str { "@" }
-        }
-
-        impl<$($t),*> ::objc::Encode for *mut $name<$($t),*> {
-            fn code() -> &'static str { "@" }
-        }
     );
 }

b4514e6ecba549960e6c8a0d210f39ad0b9290a6
Steven Sheldon <steven@sasheldon.com>
2015-07-05 22:58:41 -0700

    Added a stub app for iOS tests.

---
+++ ios-tests/RustObjCTests/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  RustObjCTests
+//
+//  Created by Steven Sheldon on 7/5/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@end
---
+++ ios-tests/RustObjCTests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RustObjCTests
+//
+//  Created by Steven Sheldon on 7/5/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}

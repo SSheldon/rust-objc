e9577ae739e4f53982d380148fe54bb0e1d253be
Steven Sheldon <steven@sasheldon.com>
2015-11-11 20:51:08 -0800

    Removed libc dependency from xtests by using std::os::raw types.

--- xtests/lib.rs
+++ xtests/lib.rs
@@ -1,4 +1,3 @@
-extern crate libc;
 #[macro_use]
 extern crate objc;
 
@@ -11,23 +10,23 @@ mod test_utils;
 
 mod tests;
 
-use libc::{c_char, size_t};
+use std::os::raw::c_char;
 use tests::TESTS;
 
 #[no_mangle]
-pub extern fn tests_count() -> size_t {
-    TESTS.len() as size_t
+pub extern fn tests_count() -> usize {
+    TESTS.len()
 }
 
 #[no_mangle]
-pub extern fn test_name(i: size_t, len: &mut size_t) -> *const c_char {
+pub extern fn test_name(i: usize, len: &mut usize) -> *const c_char {
     let (name, _) = TESTS[i as usize];
-    *len = name.len() as size_t;
+    *len = name.len();
     name.as_ptr() as *const c_char
 }
 
 #[no_mangle]
-pub extern fn run_test(i: size_t) {
+pub extern fn run_test(i: usize) {
     let (_, test_fn) = TESTS[i as usize];
     test_fn();
 }

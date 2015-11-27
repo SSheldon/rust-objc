8fc644afb1cebac9d0a3cffe1a7f97edacef8208
Steven Sheldon <steven@sasheldon.com>
2015-11-26 21:11:00 -0600

    Use gnustep package rather than compiling it.

--- travis_install.sh
+++ travis_install.sh
@@ -9,16 +9,6 @@ rust_ios_install() {
     "./${ios_stdlib}/install.sh" --prefix=$(rustc --print sysroot)
 }
 
-gnustep_install() {
-    git clone https://github.com/gnustep/libobjc2.git
-    mkdir libobjc2/build
-    cd libobjc2/build
-    export CC="clang"
-    export CXX="clang++"
-    cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/libobjc2_staging ../
-    make install
-}
-
 for arch in $IOS_ARCHS; do
     rust_ios_install "$arch"
 done
@@ -26,8 +16,4 @@ done
 if [ -n "$IOS_ARCHS" ]; then
     curl -LO https://github.com/SSheldon/rust-test-ios/releases/download/0.1.1/rust-test-ios
     chmod +x rust-test-ios
-fi
-
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    gnustep_install
 fi

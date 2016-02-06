b67803dc2b0733bc713266e7d4fba1d1ed458698
Steven Sheldon <steven@sasheldon.com>
2016-02-06 13:30:42 -0800

    Don't source the gnustep_install script.

--- gnustep_install.sh
+++ gnustep_install.sh
@@ -4,10 +4,8 @@ set -ev
 
 git clone https://github.com/gnustep/libobjc2.git
 mkdir libobjc2/build
-pushd libobjc2/build
+cd libobjc2/build
+export CC="clang"
+export CXX="clang++"
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/libobjc2_staging ../
 make install
-export CPATH=$HOME/libobjc2_staging/include:$CPATH
-export LIBRARY_PATH=$HOME/libobjc2_staging/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$HOME/libobjc2_staging/lib:$LD_LIBRARY_PATH
-popd

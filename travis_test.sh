a1116fb8a71d26b10dd53f021097e19f3e747e57
Steven Sheldon <steven@sasheldon.com>
2016-02-06 16:44:17 -0800

    Run the XCTest target as part of travis_test.
    This fixes #42.

--- travis_test.sh
+++ travis_test.sh
@@ -7,4 +7,11 @@ if [ -z "$IOS_ARCHS" ]; then
     cargo test --verbose --features "$FEATURES"
 else
     make -C "xtests" RUST_ARCHS="$IOS_ARCHS"
+    cd ios-tests
+    xcodebuild \
+        -project RustObjCTests.xcodeproj \
+        -scheme RustObjC \
+        -destination 'platform=iOS Simulator,name=iPhone 5' \
+        -destination 'platform=iOS Simulator,name=iPhone 5s' \
+        test
 fi

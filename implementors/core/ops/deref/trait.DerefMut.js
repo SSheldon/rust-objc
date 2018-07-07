7eb5624d709d25c9741d147e73f7e4d0fc054a4e
Steven Sheldon <steven@sasheldon.com>
2018-07-07 14:58:55 -0700

    Updated documentation.

---
+++ implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["block"] = [{text:"impl&lt;A, R, F&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"block/struct.ConcreteBlock.html\" title=\"struct block::ConcreteBlock\">ConcreteBlock</a>&lt;A, R, F&gt;",synthetic:false,types:["block::ConcreteBlock"]},];
+implementors["objc_id"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"objc_id/struct.Id.html\" title=\"struct objc_id::Id\">Id</a>&lt;T, <a class=\"enum\" href=\"objc_id/enum.Owned.html\" title=\"enum objc_id::Owned\">Owned</a>&gt;",synthetic:false,types:["objc_id::id::Id"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];
-implementors["objc"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='objc/struct.MessageError.html' title='objc::MessageError'>MessageError</a>",];
+implementors["objc"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"objc/struct.MessageError.html\" title=\"struct objc::MessageError\">MessageError</a>",synthetic:false,types:["objc::message::MessageError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
